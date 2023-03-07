4fc209ee59637d7a497b5329ac97c497665b27d4
TKwakernaak <43242916+TKwakernaak@users.noreply.github.com>
2023-03-07 11:47:13 +0100

    Add when then optional filters

---
+++ QuackLibs.FilterBuilder/OptionalFilter.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq.Expressions;
+
+namespace QuackLibs.FilterBuilder;
+public class OptionalFilter<T>
+{
+    private readonly Func<bool> _condition;
+    private Filter<T> _filter { get; set; }
+
+    public OptionalFilter(Filter<T> filter, Func<bool> condition)
+    {
+        _filter = filter;
+        _condition = condition;
+    }
+
+    public Expression<Func<T, bool>> Then(Expression<Func<T, bool>> expr2)
+    {
+        if (_condition())
+            _filter.And(expr2);
+
+        return _filter;
+    }
+}
