dae500ee03595938f68cc671491396e7b9e830fc
TKwakernaak <43242916+TKwakernaak@users.noreply.github.com>
2023-03-17 19:58:43 +0100

    Add service collection

---
+++ QuackLibs.FilterBuilder.DependencyInjection/FilterBuilderServiceExtensions.cs
@@ -0,0 +1,19 @@
+﻿using QuackLibs.FilterBuilder;
+
+namespace Microsoft.Extensions.DependencyInjection;
+
+/// <summary>
+/// Add the filter builder to the DI container
+/// </summary>
+public static class FilterBuilderServiceExtensions
+{
+    /// <summary>
+    /// make easily extendable filter available in your application
+    /// </summary>
+    /// <param name="services">The services collection being extended.</param>
+    public static IServiceCollection AddFilterBuilder(this IServiceCollection services)
+    {
+        services.AddScoped<IFilter, FilterBuilder>();
+        return services;
+    }
+}
